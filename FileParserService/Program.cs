0b7de3a964e12c8dab6d52dfae4341cc35a8732b
51dollar <borushkoddima@mail.ru>
2025-12-09 01:35:40 +0300

    add service folder path

--- FileParserService/Program.cs
+++ FileParserService/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FileParserService.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+string configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+builder.Services.AddSingleton(sp => new FolderPathService(
+    sp.GetRequiredService<IConfiguration>(), sp.GetRequiredService<ILogger<FolderPathService>>(), configPath));
+
+var host = builder.Build();
+
+var folderService = host.Services.GetRequiredService<FolderPathService>();
+var folderPath = await folderService.GetFolderPathAsync();
---
+++ FileParserService/Service/FolderPathService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FileParserService.Service;
+
+public class FolderPathService(IConfiguration config, ILogger<FolderPathService> logger, string configPath)
+{
+    public async Task<string> GetFolderPathAsync()
+    {
+        string? folderPath = config["FolderPath"];
+        
+        logger.LogInformation("Чтение пути из конфигурации.");
+
+        if (string.IsNullOrWhiteSpace(folderPath))
+            logger.LogWarning("Путь к папке в конфигурации отсутствует");
+        else if (!Directory.Exists(folderPath))
+            logger.LogWarning("Путь в конфигурации найден, но директория не существует.");
+
+        if (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
+        {
+            Console.WriteLine("Путь отсутствует или директория не существует.");
+            Console.Write("Введите путь к директории: ");
+            
+            folderPath = Console.ReadLine();
+            logger.LogInformation("Путь введенный пользователем: " + folderPath);
+            
+            while (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
+            {
+                logger.LogWarning("Пользователь ввёл некорректный путь: " + folderPath);
+                Console.WriteLine("Такой директории не существует. Повторите ввод.");
+                Console.Write("Введите путь: ");
+                folderPath = Console.ReadLine();
+            }
+            
+            await UpdateJsonConfigAsync(folderPath);
+            logger.LogInformation("Путь обновлен и сохранен.");
+        }
+        else
+        {
+            logger.LogInformation("Путь успешно загружен.");
+        }
+        
+        return folderPath;
+    }
+
+    private async Task UpdateJsonConfigAsync(string folderPath)
+    {
+        logger.LogInformation("Обновление файла конфигурациию.");
+        
+        var json = await File.ReadAllTextAsync(configPath);
+        var data = JObject.Parse(json);
+        
+        data["FolderPath"] = folderPath;
+        
+        await File.WriteAllTextAsync(
+            configPath, 
+            data.ToString(Formatting.Indented));
+        
+        logger.LogInformation("Файл конфигурации обновлен.");
+    }
+}
