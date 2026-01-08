6a5e6680c4226a6a34ed8c2924f0ed20b741db7f
51dollar <borushkoddima@mail.ru>
2026-01-08 09:35:26 +0300

    add migration

---
+++ DataProcessorService/Data/Migrations/20260108063234_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataProcessorService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    ModuleCategoryId = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ModuleState = table.Column<string>(type: "TEXT", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.ModuleCategoryId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Modules");
+        }
+    }
+}
